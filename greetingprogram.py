7a0e4a0d96ac9bfe006989bf82e9c8677d9e8228
Benjamin Trevor Grenier <61502669+benjamintrevorgrenier@users.noreply.github.com>
2021-05-17 14:11:23 +1000

    Create greetingprogram.py

---
+++ greetingprogram.py
@@ -0,0 +1,9 @@
+print("This program will perform a formal greeting.")
+a = (input("Enter your first name: "))
+b = (input("Enter your last name: "))
+c = ("Hi, ")
+d = a + " " + b
+e = (". How are you today?")
+f = c + d + e
+print(f)
+input("Press enter to continue.......")
