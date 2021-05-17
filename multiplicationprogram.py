79b973794ad13c48a25f96d702cc77c83b146cfb
Benjamin Trevor Grenier <61502669+benjamintrevorgrenier@users.noreply.github.com>
2021-05-17 13:37:13 +1000

    Create multiplicationprogram.py

---
+++ multiplicationprogram.py
@@ -0,0 +1,8 @@
+import math
+
+print("This program will multiply any 2 numbers")
+a = int(input("Enter first number: "))
+b = int(input("Enter second number: "))
+c = int(a * b)
+print(c)
+input("Press enter to continue.......")
