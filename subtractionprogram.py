5c33129cb6997eb9d885d4bb1304a229b11eaa2f
Benjamin Trevor Grenier <61502669+benjamintrevorgrenier@users.noreply.github.com>
2021-05-17 13:35:47 +1000

    Create subtractionprogram.py

---
+++ subtractionprogram.py
@@ -0,0 +1,8 @@
+import math
+
+print("This program will subtract one number from another ")
+a = int(input("Enter first number: "))
+b = int(input("Enter second number: "))
+c = int(a - b)
+print(c)
+input("Press enter to continue.......")
