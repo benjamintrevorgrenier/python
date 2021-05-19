15ff2da4dadba81d8e90cb49a44ed82e8d627a2c
Benjamin Trevor Grenier <61502669+benjamintrevorgrenier@users.noreply.github.com>
2021-05-19 13:57:08 +1000

    Create readandopenfiles.py

---
+++ readandopenfiles.py
@@ -0,0 +1,11 @@
+file = open('C:/users/PC56/Desktop/ggg.txt')
+file.write("This is my first file")
+Traceback (most recent call last):
+  File "<input>", line 1, in <module>
+io.UnsupportedOperation: not writable
+file.close()
+file = open('C:/users/PC56/Desktop/ggg.txt')
+file.read(10)
+'hi how are'
+file.read()
+' you'
