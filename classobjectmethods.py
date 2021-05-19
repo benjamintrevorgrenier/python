2c94392b9ef87947ebae2ebb6460006a19e104e1
Benjamin Trevor Grenier <61502669+benjamintrevorgrenier@users.noreply.github.com>
2021-05-19 13:56:15 +1000

    Create classobjectmethods.py

---
+++ classobjectmethods.py
@@ -0,0 +1,10 @@
+class calculator:
+    def add(self,a,b):
+        return a + b
+    def sub(self,a,b):
+        return a - b
+clac = calculator()
+result = clac.add(5,2)
+print(result)
+result = clac.sub(5,2)
+print(result)
