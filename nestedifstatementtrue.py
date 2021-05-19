09e9a821fe1819d9dfd62257323a9dc6bed0768f
Benjamin Trevor Grenier <61502669+benjamintrevorgrenier@users.noreply.github.com>
2021-05-19 13:51:59 +1000

    Create nestedifstatementtrue.py

---
+++ nestedifstatementtrue.py
@@ -0,0 +1,10 @@
+item = "fruit"
+fruit = "apple"
+if item == "fruits":
+    if fruit == "apple":
+        print("This fruit is an apple")
+    else:
+        print("This fruit is not available")
+
+else:
+    print("This is not a fruit")
