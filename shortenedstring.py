e6e0bea7e893abcee2327cf965a239549ba8439a
Benjamin Trevor Grenier <61502669+benjamintrevorgrenier@users.noreply.github.com>
2021-05-22 14:11:12 +1000

    Create shortenedstring.py

---
+++ shortenedstring.py
@@ -0,0 +1,5 @@
+#This program is to print out the first 3 characters entered by user
+string = input("Please enter string: ")
+string = string[0:3]
+print("Shortened string: "+ string)
+input("Press enter to exit the program")
