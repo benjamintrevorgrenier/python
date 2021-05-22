9a4d8878d43b835353e74084ca285398960f43e4
Benjamin Trevor Grenier <61502669+benjamintrevorgrenier@users.noreply.github.com>
2021-05-22 14:14:32 +1000

    Create pornstartname.py

---
+++ pornstartname.py
@@ -0,0 +1,7 @@
+#This program is to find your porn star name
+print("Lets find your porn star name")
+psname1 = input("Please enter the name of your first pet: ")
+psname2 = input("Please enter the name of the street you live on: ")
+psname3 = psname1 + " " + psname2
+print("Your porn star name is: "+ psname3)
+input("Press enter to exit the program")
