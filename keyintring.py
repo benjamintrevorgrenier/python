c6b57098f91b05b81c0a6c45b84954dca0f559c4
Benjamin Trevor Grenier <61502669+benjamintrevorgrenier@users.noreply.github.com>
2021-05-22 14:04:33 +1000

    Update keyintring.py

--- keyintring.py
+++ keyintring.py
@@ -1,7 +1,7 @@
-string = input("Enter a long string \n") 
-key = input("Enter the letter to search for: \n")
+string = input("Enter a long string: ")
+key = input("Enter the letter to search for: ")
 if(key in string):                  #Checks if key is in the string
     print("Letter found in string") #If the Bool value is TRUE
 else:
-    print("Letter not found in string \n")
-input("Press enter \n")
+    print("Letter not found in string")
+input("Press enter to exit")
